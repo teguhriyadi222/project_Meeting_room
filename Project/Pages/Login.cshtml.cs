6d8cf8b5d6389e063defd01307c6bcabe3732ba2
Adam <adamardiyantow@gmail.com>
2023-07-05 15:44:17 +0700

    finish booking features

--- Project/Pages/Login.cshtml.cs
+++ Project/Pages/Login.cshtml.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
+
 namespace Login.Pages
 {
     public class LoginModel : PageModel
--- Project/Pages/User/Booking.cshtml.cs
+++ Project/Pages/User/Booking.cshtml.cs
@@ -14,29 +14,73 @@ namespace Project.Pages.User
     public class Booking : PageModel
     {
         public SelectList? Rooms { get; set;}
-        public Event NewEvent {get; set;}
+        [BindProperty(SupportsGet = true)]
+        public string Title { get; set;}
+        [BindProperty(SupportsGet = true)]
+        public string RoomId { get; set;}
+        [BindProperty(SupportsGet = true)]
+        public DateTime StartDate { get; set;}
+        [BindProperty(SupportsGet = true)]
+        public DateTime EndDate { get; set;}
+        [BindProperty(SupportsGet = true)]
+        public string Atendee { get; set;}
+        [BindProperty(SupportsGet = true)]
+        public string Description { get; set; }
+        private string Id { get; set; }
+
         public async Task OnGetAsync()
         {
             var service = GoogleCredential.CreateCredential();
             CalendarList calendarList = service.CalendarList.List().Execute();
             IEnumerable<CalendarListEntry> roomList = calendarList.Items.Where(c => c.Description != null);
             roomList = roomList.Where(c => c.Description.Contains("Ruang"));
-            IEnumerable<string> room = roomList.Select(c => c.Summary);
+            IEnumerable<string> room = roomList.Select(c => c.Summary).Order();
 
             Rooms = new SelectList(room);
 
         }
         public async Task<IActionResult> OnPostAsync()
         {
-          if (!ModelState.IsValid || NewEvent == null)
-            {
-                return Page();
-            }
+          
 
             var service = GoogleCredential.CreateCredential();
             CalendarList calendarList = service.CalendarList.List().Execute();
+            foreach (CalendarListEntry calendar in calendarList.Items)
+            {
+                if (calendar.Summary == RoomId)
+                {
+                    Id = calendar.Id;
+                    break;
+                }
+            }
 
-            return RedirectToPage("./Index");
+
+            Event newEvent = new Event
+            {
+                Summary = Title,
+                Start = new EventDateTime
+                {
+                    DateTime = DateTime.Parse(StartDate.ToString("yyyy-MM-dd HH:mm")),
+                    TimeZone = "GMT+7"
+                },
+                End = new EventDateTime
+                {
+                    DateTime = DateTime.Parse(EndDate.ToString("yyyy-MM-dd HH:mm")),
+                    TimeZone = "GMT+7"
+                },
+                Attendees = new List<EventAttendee>()
+                {
+                    new EventAttendee()
+                    {
+                        Email = Atendee,
+                    }
+                },
+                Description = Description,
+                Location = "Jakarta"
+            };
+            service.Events.Insert(newEvent, Id).Execute();
+
+            return RedirectToPage("/User");
         } 
     }
 }
