dd12868d9e3768b6494784d11963e63a96d644b3
Adam <adamardiyantow@gmail.com>
2023-07-05 12:38:17 +0700

    add index page for admin and user

---
+++ Project/Pages/User/Index.cshtml.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using GoogleAuth;
+using Google.Apis.Calendar.v3.Data;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Project.Pages.User
+{
+    public class Index : PageModel
+    {
+        [BindProperty(SupportsGet = true)]
+        public string? SearchString { get; set;}
+        public SelectList? Rooms { get; set;}
+        [BindProperty(SupportsGet = true)]
+        public string? Room { get; set;}
+        public string ListCalendar = "&src=id.indonesian%23holiday%40group.v.calendar.google.com";
+
+
+        public async Task OnGetAsync()
+        {
+            var service = GoogleCredential.CreateCredential();
+            CalendarList calendarList = service.CalendarList.List().Execute();
+            IEnumerable<CalendarListEntry> roomList = calendarList.Items.Where(c => c.Description != null);
+            roomList = roomList.Where(c => c.Description.Contains("Ruang"));
+
+            if (!string.IsNullOrEmpty(Room))
+            {
+                // filter based on size
+                foreach (CalendarListEntry room in roomList)
+                {
+                    if (room.Description.Contains(Room))
+                    {
+                        ArrangeLink(room.Id);
+                    }
+                }
+            }
+            if(!string.IsNullOrEmpty(SearchString))
+            {
+                // filter 
+                foreach (CalendarListEntry room in roomList)
+                {
+                    if (room.Description.Contains(SearchString) || room.Summary.Contains(SearchString))
+                    {
+                        ArrangeLink(room.Id);
+                    }
+                }
+            }
+            if (string.IsNullOrEmpty(Room)&&string.IsNullOrEmpty(SearchString)) {
+                foreach (CalendarListEntry room in roomList)
+                {
+                    ArrangeLink(room.Id);
+                }
+            }
+
+        }
+
+        public void ArrangeLink(string link)
+        {
+            string src = "&src=";
+            ListCalendar = ListCalendar + src + link;
+        }
+    }
+    
+}
